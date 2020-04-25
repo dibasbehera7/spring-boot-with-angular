db09b9e0cd25b6a16921bd6968cdd0206a59e6e0
dibasbehera <dibasbehera@gmail.com>
2020-04-25 19:35:41 +0530

    Initial commit

---
+++ src/test/java/com/dibas/springboot/SpringBootAngularAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dibas.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAngularAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
